22e2a699857cdd537634c8b2893af1352a33ca3d
Luis Robles <roblescoulter@gmail.com>
2017-10-21 02:36:55 +0800

    Vision API response and results

--- lib/vision_api_info.rb
+++ lib/vision_api_info.rb
@@ -1,23 +1,43 @@
 # frozen_string_literal: true
 
 require 'http'
+require 'yaml'
+require 'base64'
 
-# Imports the Google Cloud client library
-require "google/cloud/vision"
+config = YAML.safe_load(File.read('config/secrets.yml'))
+image_path = 'spec/fixtures/demo-image.jpg'
+API_URI = 'https://vision.googleapis.com/v1/'
 
-# Your Google Cloud Platform project ID
-project_id = "soa-translatethis"
-
-# Instantiates a client
-vision = Google::Cloud::Vision.new project: project_id
-
-# The name of the image file to annotate
-file_name = "./images/cat.jpg"
-
-# Performs label detection on the image file
-labels = vision.image(file_name).labels
-
-puts "Labels:"
-labels.each do |label|
-  puts label.description
+def vision_api_path(path, config)
+  API_URI + path + '?key=' + config['vision_api_token']
 end
+
+def image_request(image_path)
+  begin
+    content = Base64.encode64(open(image_path).to_a.join)
+    requests = [{ image: { content: content }, features: [{ type: 'LABEL_DETECTION' }]}]
+    { requests: requests }
+  rescue Errno::ENOENT
+    print 'Error'
+  end
+end
+
+def call_vision_url(url, image_url)
+  HTTP.post(url, json: image_request(image_url))
+end
+
+vision_response = {}
+vision_results = {}
+
+## GOOD REPO (HAPPY)
+repo_url = vision_api_path('images:annotate', config)
+vision_response[repo_url] = call_vision_url(repo_url, image_path)
+images_annotation = vision_response[repo_url].parse
+vision_results['labels'] = images_annotation['responses'][0]['labelAnnotations']
+## BAD REPO (SAD)
+bad_image_path = 'foobar.jpg'
+bad_url = vision_api_path('images:annotate', config)
+vision_response[bad_url] = call_vision_url(bad_url, bad_image_path)
+
+File.write('spec/fixtures/vision_response.yml', vision_response.to_yaml)
+File.write('spec/fixtures/vision_results.yml', vision_results.to_yaml)
