9ba21368362454cac337dae159bac62be69a1faf
Luis Robles <roblescoulter@gmail.com>
2017-10-22 23:06:07 +0800

    Created API Basic Functionality and Label Class

--- lib/google_vision_api.rb
+++ lib/google_vision_api.rb
@@ -1,35 +1,23 @@
 require 'http'
 require 'base64'
-require_relative 'artist.rb'
-require_relative 'album.rb'
-require_relative 'track.rb'
+require_relative 'labels.rb'
 
 module GoogleVisionModule
-  # Class for Spotify API
+  # Class for Google Vision API
   class VisionAPI
-    module Errors
-      class NotFound < StandardError; end
-      class Unauthorized < StandardError; end
-      class InvalidId < StandardError; end
-    end
-
-    HTTP_ERROR = {
-      401 => Errors::Unauthorized,
-      404 => Errors::NotFound,
-      400 => Errors::InvalidId
-    }.freeze
 
     API_URI = 'https://vision.googleapis.com/v1/'.freeze
 
     def initialize(api_token, cache: {})
       @api_token = api_token
       @cache = cache
+      @labels = []
     end
 
     def labels(id)
       labels_req_url = vision_api_path(['images:annotate', id].join('/'))
-      labels_data = call_sp_url(track_req_url)
-      Track.new(track_data)
+      labels_data = call_sp_url(labels_req_url)
+      labels_data.map { |data| Label.new(data) }
     end
 
     private
@@ -39,33 +27,18 @@ module GoogleVisionModule
     end
 
     def image_request(image_path)
-      begin
-        content = Base64.encode64(open(image_path).to_a.join)
-        requests = [{ image: { content: content }, features: [{ type: 'LABEL_DETECTION' }]}]
-        { requests: requests }
-      rescue Errno::ENOENT
-        requests = [{ image: { content: '' }, features: [{ type: 'LABEL_DETECTION' }]}]
-        { requests: requests }
-      end
+      content = Base64.encode64(open(image_path).to_a.join)
+      requests = [
+        { image: { content: content }, features: [{ type: 'LABEL_DETECTION' }] }
+      ]
+      { requests: requests }
     end
 
     def call_vision_url(url, image_url)
-      result = @cache.fetch(url) do 
+      result = @cache.fetch(url) do
         HTTP.post(url, json: image_request(image_url))
       end
-      successful?(result) ? result : raise_error(result)
-    end
-
-    def raise_error(res)
-      raise(HTTP_ERROR[res['error']['status']])
-    end
-
-    def successful?(result)
-      HTTP_ERROR.keys.include?(result.code) ? false : true
-    end
-
-    def raise_error(result)
-      raise(HTTP_ERROR[result.code])
+      result
     end
   end
 end
