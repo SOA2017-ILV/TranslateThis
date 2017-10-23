213cbee98c7369e6d67cb14aea4e958210b14186
ismael-laptop <ismaelnoble@gmail.com>
2017-10-23 14:56:46 +0800

    changed config file token to reflect correct key

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,14 +12,13 @@ require_relative '../lib/google_translate_api.rb'
 require_relative '../lib/google_vision_api.rb'
 require_relative '../lib/translate_this.rb'
 
-CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
+CONFIG = YAML.safe_load(File.read('./config/secrets.yml'))
 VISION_TOKEN = CONFIG['vision_api_token']
-TRANS_TOKEN = CONFIG['google_translate_token']
+TRANS_TOKEN = CONFIG['translate_api_token']
 IMAGE = 'spec/fixtures/demo-image.jpg'.freeze
 CORRECT_VI = YAML.safe_load(File.read('spec/fixtures/vision_results.yml'))
-#CORRECT_TR = YAML.safe_load(File.read('spec/fixtures/translate_results.yml'))
-STRINGS = YAML.safe_load(File
-                         .read('./config/translatetextstrings.yml.example'))
+# CORRECT_TR = YAML.safe_load(File.read('spec/fixtures/translate_results.yml'))
+STRINGS = YAML.safe_load(File.read('./spec/fixtures/translatetextstrings.yml'))
 
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'.freeze
 CASSETTE_FILE = 'translate_this'.freeze
