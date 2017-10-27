621f88c0cf1ec19f7d496fb3861d4ce076cba814
ismael <ismaelnoble@gmail.com>
2017-10-27 14:23:35 +0800

    skeleton web app

--- app.rb
+++ app.rb
@@ -8,5 +8,30 @@ require_relative 'lib/init.rb'
 module TranslateThis
   # Web API
   class Api < Roda
+    plugin :environments
+    plugin :json
+    plugin :halt
+
+    extend Econfig::Shortcut
+    Econfig.env = environment.to_s
+    Econfig.root = '.'
+
+    route do |routing|
+      app = Api
+      config = Api.config
+
+      # GET / request
+      routing.root do
+        { 'message' => "TranslateThis API v0.1 up in #{app.environment}
+        Brought to you by TACO::DE" }
+      end
+
+      routing.on 'api' do
+        # /api/v0.1/ branch
+        routing on 'v0.1' do
+          # /api/v0.1/
+        end
+      end
+    end
   end
 end
