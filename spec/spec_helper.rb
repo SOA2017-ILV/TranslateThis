fc0a7b660256b97d9b736d9068bc6ff66dbad549
Victor Rolando Sanchez Jara <roli.s91@gmail.com>
2017-11-06 15:22:41 +0800

    Updated tests with new mapper parameters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,14 +10,23 @@ require 'minitest/rg'
 require 'rack/test'
 require 'vcr'
 require 'webmock'
+require 'ostruct'
 
 require_relative 'test_load_all.rb'
 
 CONFIG = YAML.safe_load(File.read('./config/secrets.yml'))
-GOOGLE_TOKEN = CONFIG['google_token']
 IMAGE = 'spec/fixtures/demo-image.jpg'.freeze
 CORRECT_VI = YAML.safe_load(File.read('spec/fixtures/vision_results.yml'))
 CORRECT_TR = YAML.safe_load(File.read('spec/fixtures/translation_results.yml'))
 
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'.freeze
 CASSETTE_FILE = 'translate_this'.freeze
+
+VCR.configure do |c|
+  c.cassette_library_dir = CASSETTES_FOLDER
+  c.hook_into :webmock
+
+  google_token = app.config.google_token
+  c.filter_sensitive_data('<GOOGLE_TOKEN>') { google_token }
+  c.filter_sensitive_data('<GOOGLE_TOKEN_ESC>') { CGI.escape(google_token) }
+end
--- spec/translate_this_spec.rb
+++ spec/translate_this_spec.rb
@@ -3,14 +3,6 @@
 require_relative 'spec_helper.rb'
 
 describe 'Tests TranslateThis library' do
-  VCR.configure do |c|
-    c.cassette_library_dir = CASSETTES_FOLDER
-    c.hook_into :webmock
-
-    c.filter_sensitive_data('<GOOGLE_TOKEN>') { GOOGLE_TOKEN }
-    c.filter_sensitive_data('<GOOGLE_TOKEN_ESC>') { CGI.escape(GOOGLE_TOKEN) }
-  end
-
   before do
     VCR.insert_cassette CASSETTE_FILE,
                         record: :new_episodes,
@@ -23,9 +15,9 @@ describe 'Tests TranslateThis library' do
 
   describe 'Vision information' do
     it 'HAPPY: should identify labels' do
-      vision = TranslateThis::GoogleVision::Api.new(GOOGLE_TOKEN)
+      # vision = TranslateThis::GoogleVision::Api.new(app.config)
       label_mapper = TranslateThis::GoogleVision::LabelMapper
-                     .new(vision)
+                     .new(app.config)
       labels = label_mapper.load_several(IMAGE)
       descriptions = labels.map(&:description)
       correct_descriptions = CORRECT_VI['labels'].map { |l| l['description'] }
@@ -34,26 +26,25 @@ describe 'Tests TranslateThis library' do
 
     it 'SAD: should raise exception invalid TOKEN' do
       proc do
-        vision = TranslateThis::GoogleVision::Api.new('bad_token')
-        TranslateThis::GoogleVision::LabelMapper.new(vision).load_several(IMAGE)
+        sad_config = OpenStruct.new(google_token: 'sad_token')
+        label_mapper = TranslateThis::GoogleVision::LabelMapper.new(sad_config)
+        label_mapper.load_several(IMAGE)
       end.must_raise TranslateThis::GoogleVision::Api::Errors::NotValid
     end
 
     it 'SAD should raise file not found error' do
       proc do
-        vision = TranslateThis::GoogleVision::Api.new(GOOGLE_TOKEN)
-        TranslateThis::GoogleVision::LabelMapper.new(vision)
-                                                .load_several('bad_img.jpg')
+        label_mapper = TranslateThis::GoogleVision::LabelMapper
+                       .new(app.config)
+        label_mapper.load_several('bad_img.jpg')
       end.must_raise Errno::ENOENT
     end
   end
 
   describe 'Translate information' do
     it 'HAPPY: should translate text to chinese' do
-      translation = TranslateThis::GoogleTranslation::Api
-                    .new(GOOGLE_TOKEN)
       trans_mapper = TranslateThis::GoogleTranslation::TranslateMapper
-                     .new(translation)
+                     .new(app.config)
       translate = trans_mapper.load(['Hello world'], 'zh-TW')
       correct_tr = CORRECT_TR['data']['translations'][0]['translatedText']
       _(translate.translated_text).must_equal correct_tr
@@ -61,10 +52,10 @@ describe 'Tests TranslateThis library' do
 
     it 'SAD: should raise exception invalid TOKEN' do
       proc do
-        translation = TranslateThis::GoogleTranslation::Api
-                      .new('bad_token')
-        TranslateThis::GoogleTranslation::TranslateMapper
-          .new(translation).load(['Hello world'], 'zh-TW')
+        sad_config = OpenStruct.new(google_token: 'sad_token')
+        trans_mapper = TranslateThis::GoogleTranslation::TranslateMapper
+                       .new(sad_config)
+        trans_mapper.load(['Hello world'], 'zh-TW')
       end.must_raise TranslateThis::GoogleTranslation::Api::Errors::NotValid
     end
 
